a7c0386ffa6804d3d4e2e2e4aac139fb59b5c1df
ananth-ar <ananthravi841@gmail.com>
2024-09-26 16:57:59 +0530

    26.09 o

--- server/index.ts
+++ server/index.ts
@@ -1,21 +1,36 @@
 import express, { Request, Response } from "express";
 import path from "path";
+import cors from "cors";
 
 const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use(express.static("public"));
+app.use(
+  "/client/editor",
+  express.static(path.join(__dirname, "../client/editor/dist"))
+);
+
+app.use(
+  "/client/dashboard",
+  express.static(path.join(__dirname, "../client/dashboard/dist"))
+);
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/editor/dist/index.html"));
-});
+// app.get("/", (req: Request, res: Response) => {
+//   res.sendFile(path.join(__dirname, "../client/editor/dist/index.html"));
+// });
 
-app.get("/dashboard", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dashboard/index.html"));
-});
+// app.get("/dashboard", (req: Request, res: Response) => {
+//   res.sendFile(path.join(__dirname, "../client/dashboard/dist/index.html"));
+// });
 
 app.use("/test", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript with Express!");
+  res.json({ message: "Hello, TypeScript with Express!" });
 });
 
 const PORT = process.env.PORT || 4000;
